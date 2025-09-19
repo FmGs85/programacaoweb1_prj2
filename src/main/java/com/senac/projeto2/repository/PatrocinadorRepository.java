7e189c513a21938d576fa0676854b986d00c4497
Fabio Melo <108203533+FMGSdev@users.noreply.github.com>
2025-09-19 14:36:35 -0300

    incluido participante e patrocinador repo

--- src/main/java/com/senac/projeto2/repository/PatrocinadorRepository.java
+++ src/main/java/com/senac/projeto2/repository/PatrocinadorRepository.java
@@ -1,9 +1,30 @@
 package com.senac.projeto2.repository;
 
 import com.senac.projeto2.entity.Patrocinador;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PatrocinadorRepository extends JpaRepository<Patrocinador, Integer> {
+    @Query("SELECT p FROM Patrocinador p WHERE p.status >=0")
+    List<Patrocinador> listarPatrocinadores();
+    @Query("""
+            SELECT p
+            FROM Patrocinador p
+            WHERE
+                p.id=:id AND
+                p.status>=0
+            """)
+    Patrocinador obterPatrocinadorPorId(@Param("id")int id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Patrocinador p SET p.status = -1 WHERE p.id = :id")
+    int apagadoLogico(@Param("id")int id);
 }
